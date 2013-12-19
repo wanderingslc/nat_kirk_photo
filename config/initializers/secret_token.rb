b3c3c8d65fc30c9d8d58159c888cce45a45ffd0f
Stan Martineau <wanderingslc@gmail.com>
2013-12-19 14:21:44 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NatKirkPhoto::Application.config.secret_key_base = '5e94e24f7f6c428f00612c3e31b2363acffae23a92c3d3b4f72d22edb3ba748b5627634f9bfd895269cc3d60f1225ffabb900f41bf54c2013a84727ebd2fb32a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NatKirkPhoto::Application.config.session_store :cookie_store, key: '_nat_kirk_photo_session'
